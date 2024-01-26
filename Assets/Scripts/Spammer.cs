a83b28ad009050f499f61521db4be595862338c0
izen57 <golovoslon@gmail.com>
2024-01-26 23:20:20 +0100

    add entities

---
+++ Assets/Scripts/Spammer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class Spammer: MonoBehaviour
+	{
+		public string Name { get; private set; }
+		public float Money { get; private set; }
+		public float SpamSent { get; private set; }
+		public float SpamRead { get; private set; }
+		public float ReadFrequency { get; private set; }
+		public float MoneyPerRead { get; private set; }
+
+		public Spammer(string name)
+		{
+			Name = name;
+			SpamSent = 0f;
+			SpamRead = 0f;
+			Money = 0f;
+			MoneyPerRead = 10f;
+			ReadFrequency = 5f;
+		}
+		
+		public void IncreaseSpam()
+		{
+			SpamSent++;
+		}
+
+		public void IncreaseMoney()
+		{
+			Money += MoneyPerRead;
+		}
+
+		public void OnSpamRead()
+		{
+			IncreaseMoney();
+		}
+	}
+}
