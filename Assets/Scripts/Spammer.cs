4b0f3e5d7c45fde85e035da5733a7a8f4150c348
izen57 <golovoslon@gmail.com>
2024-01-27 00:35:50 +0100

    change frequence determination

--- Assets/Scripts/Spammer.cs
+++ Assets/Scripts/Spammer.cs
@@ -6,16 +6,20 @@ namespace Assets.Scripts
 	{
 		public string Name { get; private set; }
 		public float Money { get; private set; }
-		public float SpamSent { get; private set; }
-		public float SpamRead { get; private set; }
+		public int SpamSent { get; private set; }
+
+		public int SpamRead { get; private set; }
+		private int _internalSpamReadCounter;
+
 		public float ReadFrequency { get; private set; }
 		public float MoneyPerRead { get; private set; }
 
 		public void Start()
 		{
 			Name = name;
-			SpamSent = 0f;
-			SpamRead = 0f;
+			SpamSent = 0;
+			SpamRead = 0;
+			_internalSpamReadCounter = 0;
 			Money = 0f;
 			MoneyPerRead = 10f;
 			ReadFrequency = 5f;
@@ -33,7 +37,10 @@ namespace Assets.Scripts
 
 		public void OnSpamRead()
 		{
-			IncreaseMoney();
+			if (++_internalSpamReadCounter % ReadFrequency == 0) {
+				IncreaseMoney();
+				++SpamRead;
+			}
 		}
 	}
 }
