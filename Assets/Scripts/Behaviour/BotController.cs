d4a3c8827b17ee6099d432e7fc0e8f6d9caed52c
Tulenien <timofey.evsigneev@gmail.com>
2024-01-27 15:17:20 +0100

    feat: add bot entity

--- Assets/Scripts/Behaviour/BotController.cs
+++ Assets/Scripts/Behaviour/BotController.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BotController : MonoBehaviour
 {
+    public BotSO botSO;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SendLetter(botSO.time));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SendLetter(float time)
+    {
+        yield return new WaitForSeconds(time);
+        while (true)
+        {
+            Debug.Log("Send letter with bot");
+            yield return new WaitForSeconds(time);
+        }
     }
 }
---
+++ Assets/Scripts/Behaviour/BotSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BotSo", menuName = "Scriptable Objects/ Bot")]
+public class BotSO : ScriptableObject
+{
+    public float time;
+    public int letters;
+}
