a6bdc1d8717303486c1c43680b44a9ecf1ef55c2
izen57 <golovoslon@gmail.com>
2024-01-27 12:54:43 +0100

    feat: change spammer

--- Assets/Scripts/Spammer.cs
+++ Assets/Scripts/Spammer.cs
@@ -5,7 +5,6 @@ namespace Assets.Scripts
 	public class Spammer: MonoBehaviour
 	{
 		public string Name { get; private set; }
-		public int Level { get; private set; }
 		public float Money { get; private set; }
 		public int SpamSent { get; private set; }
 
@@ -20,13 +19,12 @@ namespace Assets.Scripts
 			Debug.Log("The spammer has been started.");
 
 			Name = name;
-			Level = 1;
 			SpamSent = 0;
 			SpamRead = 0;
 			_internalSpamReadCounter = 0;
 			Money = 0f;
-			MoneyPerRead = 10f;
-			ReadFrequency = 5f;
+			MoneyPerRead = 0.2f;
+			ReadFrequency = 10f;
 
 			GetComponent<Renderer>().enabled = false;
 		}
