99fadb62b18075fef89912aea56cf41915b40436
izen57 <golovoslon@gmail.com>
2024-01-28 13:51:46 +0100

    rename the bot button and its script

--- Assets/Scripts/Menus/IncreaseBotFrequencyButtonScript.cs
+++ Assets/Scripts/Menus/IncreaseBotFrequencyButtonScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class IncreaseSpamBotFrequencyButtonScript: MonoBehaviour
+public class IncreaseBotFrequencyButtonScript: MonoBehaviour
 {
 	[SerializeField] Button m_button;
 
